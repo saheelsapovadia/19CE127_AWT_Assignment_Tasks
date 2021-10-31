d2fe64c3f3613fb6bcd4532f3c9b30b0239ab682
saheelsapovadia <59962703+saheelsapovadia@users.noreply.github.com>
2021-10-31 21:04:41 +0530

    Assingments

---
+++ CRUD_Bootstrap_Express_Mysql/app.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root@3011",
+  database: "crudmysql",
+});
+
+connection.connect(function (error) {
+  if (!!error) console.log(error);
+  else console.log("Database Connected!");
+});
+
+//set views file
+app.set("views", path.join(__dirname, "views"));
+
+//set view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  let sql = "SELECT * FROM university";
+  let query = connection.query(sql, (err, rows) => {
+    if (err) throw err;
+    res.render("user_index", {
+      title: "CRUD Operation using ExpressJS MySQL",
+      users: rows,
+    });
+  });
+});
+
+app.get("/add", (req, res) => {
+  res.render("user_add", {
+    title: "CRUD Operation using ExpressJS MySQL",
+  });
+});
+
+app.post("/save", (req, res) => {
+  let data = {
+    cid: req.body.cid,
+    cname: req.body.cname,
+    dname: req.body.dname,
+    iname: req.body.iname,
+    uname: req.body.uname,
+  };
+  let sql = "INSERT INTO university SET ?";
+  let query = connection.query(sql, data, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.get("/edit/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let sql = `Select * from university where id = ${userId}`;
+  let query = connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("user_edit", {
+      title: "CRUD Operation using ExpressJS MySQL",
+      user: result[0],
+    });
+  });
+});
+
+app.post("/update", (req, res) => {
+  const userId = req.body.id;
+  let sql =
+    "update university SET cid='" +
+    req.body.cid +
+    "', cname='" +
+    req.body.cname +
+    "',  dname='" +
+    req.body.dname +
+    "', iname='" +
+    req.body.iname +
+    "', uname='" +
+    req.body.uname +
+    "' where id =" +
+    userId;
+  let query = connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.get("/delete/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let sql = `DELETE from university where id = ${userId}`;
+  let query = connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+// Server Listening
+app.listen(3030, () => {
+  console.log("Server is running at port 3030");
+});
